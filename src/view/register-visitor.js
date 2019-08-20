261dae29ba2194bf61055c08736860aafd26ad86
Daniela Gutierrez <danieladelcarmen168@hotmail.com>
2019-08-20 16:04:32 -0500

    Contador de acompañantes

--- src/view/register-visitor.js
+++ src/view/register-visitor.js
@@ -13,7 +13,9 @@ export default () => {
     <input type="mail" id="email" placeholder="Correo electrónico" autocomplete="on" required>
     <input type="text" id="company" placeholder="Compañía" autocomplete="on" required>
     <input type="text" required id="host" placeholder="Host" autocomplete="on">
-    <p>Acompañantes: </p> <button>+</button><button>-</button>
+    <p>Acompañantes: <button id="mas">+</button>
+    <button id="menos">-</button>
+    <p id="demo"></p>
     <button id="register-btn">Registrarse</button>
   </form>
   
@@ -21,10 +23,18 @@ export default () => {
   createChildNode.innerHTML = content;
 
 
+  let a = 0;
+  const aumenta = createChildNode.querySelector("#mas");
+  aumenta.addEventListener('click', () => {
+    a = a + 1;
+    document.getElementById("demo").textContent = a;
+  })
 
-
-
-
+  const disminuye = createChildNode.querySelector("#menos");
+  disminuye.addEventListener('click', () => {
+    a = a - 1;
+    document.getElementById("demo").textContent = a;
+  })
 
   const registerBtn = createChildNode.querySelector('#register-btn');
   registerBtn.addEventListener('click', (event) => {
