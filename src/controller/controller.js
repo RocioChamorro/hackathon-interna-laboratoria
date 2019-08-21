aba5382d7b0f6d3cb51abdb9b44a331817d39283
Rocio <chamorrorocio5@gmail.com>
2019-08-21 00:12:19 -0500

    session

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -3,13 +3,11 @@ export const register = () => {
   const name = document.getElementById('name').value;
   const email = document.getElementById('email').value;
   const identification = document.getElementById('identification').value;
-  const identityNumber = document.getElementById('identity').value;
   const company = document.getElementById('company').value;
   const host = document.getElementById('host').value;
 
-  dataBase(name, email, identity, company, host)
+  dataBase(name, email, identification, company, host)
 }
-
 
 export const dataBase = (name, email, identity, company, host) => {
   const obj = {
--- src/view-controller/index.js
+++ src/view-controller/index.js
@@ -2,6 +2,7 @@ import Home from '../view/home.js'
 import Register from "../view/register-visitor.js"
 import Photo from "../view/photo.js"
 import Profile from "../view/profile.js"
+import LoginAdmin from "../view/loginAdmin.js"
 import Admin from "../view/admin.js"
 // import Body from '../view/body.js'
 const components = {
@@ -9,6 +10,7 @@ const components = {
    register: Register,
    photo: Photo,
    profile: Profile,
+   loginAdmin: LoginAdmin,
    admin: Admin
   //  header: Header,
   //  body: Body
